16d9eed82db14521800c98e7c780f91347fe737d
student <mika89_11@tlen.pl>
2019-10-05 09:37:50 +0200

    pierwszy projekt

---
+++ Pierwszy/Pierwszy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pierwszy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Imię: "); //Your name
+            Console.WriteLine("Janusz:");
+            /*
+             * komentarz
+             * w 
+             * wielu
+             * liniach
+            */
+
+            string name = "Anna";
+            Console.WriteLine(name);
+            // + konkatenacja
+            Console.WriteLine("Masz na imię " + name);
+            Console.WriteLine("Masz na imię ");
+            Console.WriteLine(name);
+            Console.WriteLine("Masz na imię : {0}", name);
+            int age = 20;
+            sbyte age1 = 20;
+             Console.WriteLine("wiek: {0}, imię: {1}", age, name);
+             Console.WriteLine("wiek1: {0}, imię: {1}", age1, name);
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
